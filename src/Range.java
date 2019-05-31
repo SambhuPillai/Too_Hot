05f1bff6ac2d69c43296d1a7ae0ff6881ac9f118
Sam <spillai395@gmail.com>
2019-05-31 09:36:30 +0100

    hi

---
+++ src/Range.java
@@ -0,0 +1,13 @@
+
+public class Range {
+	public static boolean temperature(int a) {
+		boolean isSummer;
+		if (a>=60 & a<=100) {
+			isSummer=true;
+			return isSummer;
+		}
+		else isSummer=false;
+		return isSummer;
+	}
+
+}
---
+++ src/RunningTH.java
@@ -0,0 +1,9 @@
+
+public class RunningTH {
+
+	public static void main(String[] args) {
+		boolean result = Range.temperature(60);
+		System.out.println(result);
+	}
+
+}
